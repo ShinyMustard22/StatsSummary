fa658cc052c905635b4701f5a80a1ecdc57f4830
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-13 14:16:13 -0700

    Added more functions.

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ def main():
         print("Sorry, you need at least 2 data values.")
         return
 
-    print("Population standard deviation:", statistics.stdev(numbers))
+    numbers.sort()
+
+    print("Mean:", statistics.mean(numbers))
+    print("Median:", statistics.median(numbers))
+    print("Mode:", statistics.mode(numbers))
+    print("Range:", numbers[-1] - numbers[0])
+
+    print("Population variance:", statistics.pvariance(numbers))
+    print("Sample variance:", statistics.variance(numbers))
+
     print("Sample standard deviation:", statistics.pstdev(numbers))
+    print("Population standard deviation:", statistics.stdev(numbers))
 
 
 if __name__ == "__main__":
