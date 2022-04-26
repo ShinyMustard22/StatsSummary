df579c3b0675fc6ff7de3d12ad29e905545c7370
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-25 21:46:31 -0700

    Rounded to two decimal places.

--- main.py
+++ main.py
@@ -4,28 +4,26 @@ import statistics
 def main():
     numbers = []
 
-    num = input("Enter next number: ")
+    num = input("Enter the first number: ")
 
     while num.isdigit():
         numbers.append(int(num))
-        num = input("Enter next number: ")
+        num = input("Enter next number (enter non-numerical data to end): ")
 
     if len(numbers) < 2:
         print("Sorry, you need at least 2 data values.")
         return
 
-    numbers.sort()
+    print("Mean:", round(statistics.mean(numbers), 2))
+    print("Median:", round(statistics.median(numbers), 2))
+    print("Mode:", round(statistics.mode(numbers), 2))
+    print("Range:", round(max(numbers) - min(numbers), 2))
 
-    print("Mean:", statistics.mean(numbers))
-    print("Median:", statistics.median(numbers))
-    print("Mode:", statistics.mode(numbers))
-    print("Range:", numbers[-1] - numbers[0])
+    print("Population variance:", round(statistics.pvariance(numbers), 2))
+    print("Sample variance:", round(statistics.variance(numbers), 2))
 
-    print("Population variance:", statistics.pvariance(numbers))
-    print("Sample variance:", statistics.variance(numbers))
-
-    print("Population standard deviation:", statistics.pstdev(numbers))
-    print("Sample standard deviation:", statistics.stdev(numbers))
+    print("Population standard deviation:", round(statistics.pstdev(numbers), 2))
+    print("Sample standard deviation:", round(statistics.stdev(numbers), 2))
 
 
 if __name__ == "__main__":
